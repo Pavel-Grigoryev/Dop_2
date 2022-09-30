bb3c6403363e60edc8d2cc8005ad79b875853307
Pavel <designdpcompany@gmail.com>
2022-09-30 19:26:01 +0300

    com 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {json} from "stream/consumers";
+
+type PropsType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todos, setTodos] = useState<PropsType[]>([]);
+    console.log(todos);
+
+    useEffect(()=>{ fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => setTodos(json))}, [])
+
+    const onClickHandler = () => {
+        setTodos([]);
+    }
+
+    return (
+        <div className="App">
+            <button onClick={onClickHandler}>Clean posts</button>
+            <ul>
+                {todos.map(el => {
+                    return (<li>
+                        <span>{el.id}    </span>
+                        <span>{el.id}    </span>
+                        <span>{el.title}     </span>
+                    </li>)
+                })}
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
